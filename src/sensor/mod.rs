96516ddd8d419d9dc8c8607c32d2cb0e296f5a8f
kaicen-x <kaicenxiong@gmail.com>
2025-11-12 20:10:49 +0800

    新增PWM开关控制测试程序

--- src/sensor/mod.rs
+++ src/sensor/mod.rs
@@ -1,8 +1,9 @@
-pub mod dht11;
-pub mod button;
-pub mod led;
-pub mod hx711;
-pub mod switch;
 pub mod aht30;
 pub mod bme280;
+pub mod button;
+pub mod dht11;
+pub mod hx711;
+pub mod led;
+pub mod pwm_switch;
+pub mod switch;
 pub mod uln2003a;
